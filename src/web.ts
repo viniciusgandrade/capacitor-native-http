a5367c343213b51bbd90d356b75818d1062eb382
Vinícius Gonçalves de Andrade <viniciusgandrade@gmail.com>
2023-03-14 23:33:43 -0300

    retirando erro da web

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,8 @@ import type { HttpNativePlugin } from './definitions';
 
 export class HttpNativeWeb extends WebPlugin implements HttpNativePlugin {
   initialize(_options: { hostname: string; certPath: string; timeout: number; }): Promise<{ data: any; }> {
-    throw new Error('Method not implemented.');
+    console.log('Ignorando web.');
+    return new Promise((resolve => resolve({} as any)));
   }
   request(_options: { method: string, url: string; params?: any; data?: any; headers?: any }): Promise<{ data: any }> {
     return Promise.resolve({ data: undefined });
